e6997def002f8abea1672213e710d186bd8cbdb1
Pietromuresu <muresupietro66@gmail.com>
2023-09-21 12:50:40 +0200

    Es 1 create wishList and then print it

--- src/org/java/wishlist/first/Main.java
+++ src/org/java/wishlist/first/Main.java
@@ -1,5 +1,31 @@
 package org.java.wishlist.first;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<String> wishList = new ArrayList<>();
+		
+		
+		System.out.println("Insert a present (or \"close\" ): ");
+		String newPresent = sc.nextLine();
+		
+		while(!newPresent.toLowerCase().equals("close")) {
+			
+			wishList.add(newPresent);
+			
+			System.out.println("List length: " + wishList.size());
+			System.out.println("Insert another present (or \"close\" ): ");
+			newPresent = sc.nextLine();
+			
+		}
+		
+		System.out.println(wishList);
+		
+	}
 }
