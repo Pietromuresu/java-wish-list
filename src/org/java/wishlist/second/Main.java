200857a2d850f2f2c3e0747f8c4d7342a5c5b7bd
Pietromuresu <muresupietro66@gmail.com>
2023-09-24 19:03:44 +0200

    Es 2 - Completed

--- src/org/java/wishlist/second/Main.java
+++ src/org/java/wishlist/second/Main.java
@@ -8,25 +8,42 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-	
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Type the word to find occurences: ");
 		String givenWord = sc.nextLine();
 		
 		Map<Character, Integer> occurrences = new HashMap<>();
+
 		
-		 for (char letter : givenWord.toCharArray()) {
-			 if(!occurrences.containsKey(letter)) {
+		// Without lambdas
+//		
+//		 for (char letter : givenWord.toCharArray()) {
+//			 if(!occurrences.containsKey(letter)) {
+//		
+//				 occurrences.put(letter, 1);
+//			 }else {
+//			
+//				 Integer increaseCounter = occurrences.get(letter) + 1;
+//				 occurrences.put(letter, increaseCounter);
+//			 }
+//		}	
+//	
+//		 System.out.println(occurrences);
 		
-				 occurrences.put(letter, 1);
-			 }else {
+		
+		
+		// With lambdas
+		givenWord.chars().forEach(letter ->{
 			
-				 Integer increaseCounter = occurrences.get(letter) + 1;
-				 occurrences.put(letter, increaseCounter);
-			 }
-		}	
-	
+			if(!occurrences.containsKey((char)letter)) {
+				 occurrences.put((char)letter, 1);
+			}else {
+				 occurrences.put((char)letter, occurrences.get((char)letter) + 1);
+			}
+			
+		});
+		
 		 System.out.println(occurrences);
 	}
 }
