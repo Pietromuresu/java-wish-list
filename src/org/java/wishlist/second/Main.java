5ed9db743ffd059d875faef85945252dd82c76fc
Pietromuresu <muresupietro66@gmail.com>
2023-09-24 18:30:40 +0200

    Es 2 - count letters and push in an HashMap

--- src/org/java/wishlist/second/Main.java
+++ src/org/java/wishlist/second/Main.java
@@ -1,5 +1,32 @@
 package org.java.wishlist.second;
 
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Type the word to find occurences: ");
+		String givenWord = sc.nextLine();
+		
+		Map<Character, Integer> occurrences = new HashMap<>();
+		
+		 for (char letter : givenWord.toCharArray()) {
+			 if(!occurrences.containsKey(letter)) {
+		
+				 occurrences.put(letter, 1);
+			 }else {
+			
+				 Integer increaseCounter = occurrences.get(letter) + 1;
+				 occurrences.put(letter, increaseCounter);
+			 }
+		}	
+	
+		 System.out.println(occurrences);
+	}
 }
